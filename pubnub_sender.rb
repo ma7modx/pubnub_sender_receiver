c74051de6a36fd7c3a6c681b9c4fcf102fd9618d
mahmoud_mohammed_afify <ma7modx@hotmail.com>
2016-08-08 12:15:31 +0000

    pubnub_sender.rb edited online with Bitbucket

--- pubnub_sender.rb
+++ pubnub_sender.rb
@@ -1,7 +1,6 @@
 require 'pubnub'
 require 'byebug'
 
-debugger
 pubnub = Pubnub.new(
   subscribe_key: 'sub-c-283f45ac-27fb-11e6-9327-02ee2ddab7fe',
   publish_key: 'pub-c-5f64e0ec-ba09-4b2f-a964-82cd206f6794',
